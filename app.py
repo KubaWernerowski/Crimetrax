d269ecee8f46cce25697ec8e67e5e0467fb37084
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-03-09 17:59:35 -0500

    ljdshkjhgsdf

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from PopulateMap import PopulateMap
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 MapNodes = PopulateMap()
 
