4eee3a4cf3108a8884618232fc0e07ad676cee1f
Thomas Liu <tommyliu9@gmail.com>
2019-03-09 16:16:18 -0500

    DUmb CoDe Lmao

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import FileParser as fp
 import numpy as np
+import math
+from Node import Node
+from NeuralNetwork import Neural_Network
 dayToIndex = {'Monday    ': 1,
             'Tuesday   ': 2,
             "Wednesday ": 3,
@@ -7,17 +10,39 @@ dayToIndex = {'Monday    ': 1,
             "Friday    ": 5,
             "Saturday  ": 6,
             "Sunday    ": 7}
+'''
+
+'''
+def applyFilter(set, day, time):
+    first =  set[set.occurrencedayofweek == day]
+    first = first[first.occurrencehour == time]
+    return first
+'''
+'''
+def distanceTo(n1, n2):
+    return math.sqrt((n1.lat-n2.lat)**2 + (n1.long - n2.long)**2)
+
+def getNeighbors(Nodes, threshold):
+    for i in range(len(Nodes)):
+        for b in range(i + 1, len(x)):
+            if (distanceTo(x[i], x[b]) < threshold):
+                x[i].neighbors = x[i].neighbors + 1
 
 if __name__ == "__main__":
-    robDset = fp.parseCSV(fp.crime_datasets[1])
+
+    set = fp.parseCSV(fp.crime_datasets[1])
     features = ["LAT", "LONG", "occurencehour", "occurrencedayofyear","reporteddayofweek"]
-    msk = np.random.rand(len(robDset)) < .8
-    trainSet = robDset[msk]
-    testSet = robDset[~msk]
-    input = ["occurrencedayofweek","occurrencehour"]
-    output = ["X","Y"]
-    inputX = trainSet[input]
-    outputY = trainSet[output]
+    input = ["occurrencedayofweek","occurrencehour", "X", "Y"]
+    output = ["Lat","Long"]
+    inputX = set[input]
     inputX["occurrencedayofweek"] = inputX["occurrencedayofweek"].replace(dayToIndex)
-    for i in inputX["occurrencedayofweek"]:
-        print(i)
+
+    modified = applyFilter(inputX, 4.0, 23.0)
+    x = []
+    for index,row in modified.iterrows():
+        x.append(Node(float(row["X"]),float(row["Y"])))
+    counter = 0
+    dict = {}
+    getNeighbors(x, .03)
+    for i in x:
+        print(i.neighbors)
