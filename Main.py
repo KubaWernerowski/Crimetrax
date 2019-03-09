f2721a03ec52f41d5fba150d5b2decc8c74345cd
Thomas Liu <tommyliu9@gmail.com>
2019-03-09 16:48:24 -0500

    eskedit

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import FileParser as fp
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 from Node import Node
 from NeuralNetwork import Neural_Network
 dayToIndex = {'Monday    ': 1,
@@ -21,7 +22,8 @@ def applyFilter(set, day, time):
 '''
 def distanceTo(n1, n2):
     return math.sqrt((n1.lat-n2.lat)**2 + (n1.long - n2.long)**2)
-
+'''
+'''
 def getNeighbors(Nodes, threshold):
     for i in range(len(Nodes)):
         for b in range(i + 1, len(x)):
@@ -30,19 +32,24 @@ def getNeighbors(Nodes, threshold):
 
 if __name__ == "__main__":
 
-    set = fp.parseCSV(fp.crime_datasets[1])
+    set = fp.parseCSV(fp.crime_datasets[2])
     features = ["LAT", "LONG", "occurencehour", "occurrencedayofyear","reporteddayofweek"]
     input = ["occurrencedayofweek","occurrencehour", "X", "Y"]
     output = ["Lat","Long"]
     inputX = set[input]
     inputX["occurrencedayofweek"] = inputX["occurrencedayofweek"].replace(dayToIndex)
 
-    modified = applyFilter(inputX, 4.0, 23.0)
+    modified = applyFilter(inputX, 4.0, 11.0)
     x = []
     for index,row in modified.iterrows():
         x.append(Node(float(row["X"]),float(row["Y"])))
     counter = 0
     dict = {}
     getNeighbors(x, .03)
+    xax = []
+    yax = []
     for i in x:
-        print(i.neighbors)
+        xax.append(i.lat)
+        yax.append(i.long)
+    plt.plot(xax,yax,'ro')
+    plt.show()
